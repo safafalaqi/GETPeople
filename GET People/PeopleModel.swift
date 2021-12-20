fe5022f9636059dc23257ea5c1d987cd52ed8795
Safa Falaqi <sfalaqi@gmail.com>
2021-12-21 01:49:22 +0300

    Initial commit

---
+++ GET People/PeopleModel.swift
@@ -0,0 +1,69 @@
+//
+//  PeopleModel.swift
+//  GET People
+//
+//  Created by Safa Falaqi on 21/12/2021.
+//
+
+import Foundation
+
+// MARK: - Welcome
+struct People: Codable {
+    let count: Int
+    let next: String
+    let previous: JSONNull?
+    let results: [Result]
+}
+
+// MARK: - Result
+struct Result: Codable {
+    let name, height, mass, hairColor: String
+    let skinColor, eyeColor, birthYear: String
+    let gender: Gender
+    let homeworld: String
+    let films, species, vehicles, starships: [String]
+    let created, edited: String
+    let url: String
+
+    enum CodingKeys: String, CodingKey {
+        case name, height, mass
+        case hairColor = "hair_color"
+        case skinColor = "skin_color"
+        case eyeColor = "eye_color"
+        case birthYear = "birth_year"
+        case gender, homeworld, films, species, vehicles, starships, created, edited, url
+    }
+}
+
+enum Gender: String, Codable {
+    case female = "female"
+    case male = "male"
+    case nA = "n/a"
+}
+
+// MARK: - Encode/decode helpers
+
+class JSONNull: Codable, Hashable {
+
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+
+    public var hashValue: Int {
+        return 0
+    }
+
+    public init() {}
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if !container.decodeNil() {
+            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
+}
---
+++ GET People/PeopleViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  GET People
+//
+//  Created by Safa Falaqi on 21/12/2021.
+//
+
+import UIKit
+
+class PeopleViewController: UITableViewController {
+
+    var peopleList: [Result]? = []
+    var url = URL(string: "https://swapi.dev/api/people/?format=json")
+    var nextPage = true
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+      
+        loadData()
+        tableView.dataSource = self
+        
+    }
+    
+    func loadData(){
+  
+        //to get all people from different pages
+        for i in 1...8{
+            
+         self.nextPage = false
+         print(i)
+    
+        // create a URLSession to handle the request tasks
+               let session = URLSession.shared
+        // create a "data task" to make the request and run completion handler
+                let task = session.dataTask(with: url!, completionHandler: {
+                    // see: Swift closure expression syntax
+                    data, response, error in
+                    // data -> JSON data, response -> headers and other meta-information, error-> if one occurred
+                    // "do-try-catch" blocks execute a try statement and then use the catch statement for errors
+                    do {
+                        //optional unwraping
+                        guard let myData = data else {return}
+                        
+                        let decoder = JSONDecoder()
+                        
+                        let jesonResult = try decoder.decode(People.self, from: myData)
+                        
+                        self.peopleList?.append(contentsOf: jesonResult.results)
+                        
+                        DispatchQueue.main.async {
+                         self.tableView.reloadData()
+                        }
+                        
+                        if !jesonResult.next.isEmpty{
+                            self.nextPage = true
+                            self.url = URL(string: jesonResult.next)
+                        }
+                     
+                    } catch {
+                        print(error)
+                    }
+                })
+        
+        // execute the task and wait for the response before
+               // running the completion handler. This is async!
+               task.resume()
+            
+        }
+    }
+
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCell(withIdentifier: "peopleCell",for : indexPath)
+        cell.textLabel?.text = peopleList?[indexPath.row].name ?? "No data"
+        
+        return cell
+        
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return peopleList?.count ?? 0
+    }
+}
+
